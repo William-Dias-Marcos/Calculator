93bfedf4804a9e560295a01ab0d1fcc1e8a7fb54
William-Dias-Marcos <william.diasmarcos@gmail.com>
2021-12-16 20:23:48 -0300

    Part 02

---
+++ scripts/script.js
@@ -0,0 +1,24 @@
+function insert (num){
+
+    let digit = document.getElementById('viewfinder').value
+    document.getElementById('viewfinder').value = digit + num
+}
+
+function clean (){
+    document.getElementById('viewfinder').value = ""
+}
+
+function back (){
+    var dell = document.getElementById('viewfinder').value
+    document.getElementById('viewfinder').value = dell.substring(0, dell.length -1)
+}
+
+function calculate (){
+    let  result = document.getElementById('viewfinder').value
+
+    if(result){
+        document.getElementById('viewfinder').value = eval(result)
+    } else{
+        document.getElementById('viewfinder').value = ""
+    }
+}
